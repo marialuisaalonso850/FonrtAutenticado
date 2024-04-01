13f7d363826db4282cebb425053cfd980e2c9daf
marialuisaalonso850 <marialuisaalonso850@gmail.com>
2024-04-01 07:12:27 -0500

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Login from '../src/routes/Login';
+import Signup from '../src/routes/signup'; 
+import Dashboard from '../src/routes/dashboard'; 
+import ProtectedRoute from '../src/routes/protectedRaute'; 
+import { AuthProvider } from '../src/Autenticacion/AutProvider'; 
+import Home from '../src/components/Home';
+import Perfil from '../src/routes/perfil';
+import ContactUs from '../src/routes/contactUs';
+
+import Post from '../src/pages/ParqueaderoF/ParqueaderoForm';
+import Posts from '../src/pages/DatosFrom/DatosForm';
+import PostInfo from '../src/pages/DatosFrom/PostsInfo';
+import Explicacion from '../src/routes/Explicacion';
+import Reservas from '../src/routes/Reservas';
+import UserReseve from './routes/UserReserve';
+
+import ExplicacionUser from '../src/routes/ExplicacionUser';
+
+const router = createBrowserRouter([
+  {
+    path: "/Login",
+    element: <Login/>
+  },
+  {
+    path: "/signup",
+    element: <Signup/>
+  },
+  {
+    path: "/",
+    element: <Home/>
+  },
+ 
+  {
+    path: "/perfil",
+    element: <Perfil />
+  },
+  {
+    path: "/contactUs",
+    element: <ContactUs />
+  },
+  {
+    path: "/explicacion",
+    element: <Explicacion />
+  },
+  {
+    path: "/explicacionUser",
+    element: <ExplicacionUser />
+  },
+  {
+    path: "/",
+    element: <ProtectedRoute/>,
+    children:[
+      {
+        path: "/dashboard",
+        element: <Dashboard/>
+      },
+       {
+    path:"/posts",
+    element:<Posts />
+  },
+  {
+    path:"/post/:id",
+    element: <Post />
+  },
+  {
+    path: "/post/:id/info",
+    element: <PostInfo />
+  },
+  
+  {
+    path: "/reservas",
+    element: <Reservas />
+  },
+  {
+    path: "/reservasUser",
+    element: <UserReseve />
+  },
+      
+    ]
+  },
+])
+ReactDOM.createRoot(document.getElementById('root') ).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
+  </React.StrictMode>,
+)
+
+
---
+++ client/src/routes/UserReserve.jsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Button } from 'reactstrap';
+import PortalLayout from "../layout/PortalLayout";
+import Footer from "../components/Footer";
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import puestos from '../puestos.json'
+import { FaFilePdf } from "react-icons/fa";
+import { useAuth } from '../Autenticacion/AutProvider';
+import Swal from 'sweetalert2'; // Importa sweetalert2
+import '../pages/DatosFrom/info.css';
+
+const UserReseve = () => {
+  const navigate = useNavigate();
+  const [reservas, setReservas] = useState([]);
+  const auth = useAuth();
+
+  useEffect(() => {
+    const fetchReseva = async () => {
+      try {
+        const response = await fetch(`${puestos.apiUrl}`, {
+          headers: {
+            Authorization: `Bearer ${auth.getAccessToken()}`
+          }
+        });
+        if (response.ok) {
+          const data = await response.json();
+          const filteredPosts = data.filter(reserva => reserva.userId === auth.getUser().id);
+          
+          setReservas(filteredPosts);
+        } else {
+          console.error("Error al obtener los mensajes:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error al obtener los mensajes:", error);
+      }
+    };
+
+    fetchReseva();
+  }, [auth]);
+
+  const handleCheckout = (reserva) => {
+    // Crear un nuevo documento PDF
+    const doc = new jsPDF();
+
+    // Cargar la imagen del logo como base64
+    const imgData = "https://i.ibb.co/wQYC7K6/images-jpg-Photoroom-png-Photoroom.png";
+    
+    // Diseño del PDF
+    doc.setFontSize(18);
+    doc.text(80, 20, 'Detalles de la reserva', null, null, 'center');
+    
+    // Insertar el logo en el PDF
+    doc.addImage(imgData, 'PNG', 15, 15, 30, 30);
+    
+    doc.setFontSize(12);
+    doc.text(20, 60,` Fecha de reserva: ${reserva.date}`);
+    doc.text(20, 70, `Nombre: ${reserva.nombre}`);
+    doc.text(20, 80, `Hora de reserva: ${reserva.time}`);
+    
+    // Construir el array de datos para la tabla
+    const data = [
+      [reserva.date, reserva.time, reserva.nombre, reserva.placa, reserva.telefono]
+    ];
+    
+    doc.autoTable({
+      head: [['Fecha', 'Hora', 'Nombre', 'Placa', 'Numero Telefonico']],
+      body: data,
+      startY: 100
+    });
+
+    const totalPages = doc.internal.getNumberOfPages();
+    for (let i = 1; i <= totalPages; i++) {
+      doc.setPage(i);
+      const city = "Ciudad";
+      const department = "Departamento";
+      const year = new Date().getFullYear();
+      const thankYouText = `¡Gracias por reservar en nuestro parqueadero en ${city}, ${department} ${year}!`;
+      doc.text(thankYouText, 14, doc.internal.pageSize.height - 10);
+    }
+
+    doc.save("reserva.pdf");
+  };
+  
+  
+
+  const handleDelete = async (reservaId) => {
+    // Mostrar una alerta de confirmación antes de eliminar
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "Esta acción no se puede deshacer",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminarlo!',
+      cancelButtonText: 'Cancelar'
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await axios.delete(`https://rolesautenticado.onrender.com/api/reserva/cancel/${reservaId}`, {
+            headers: {
+              Authorization: `Bearer ${auth.getAccessToken()}`
+            }
+          });
+          Swal.fire(
+            'Eliminado!',
+            'La reserva ha sido eliminada.',
+            'success'
+          );
+  
+          // Actualizar la lista de reservas después de la eliminación
+          setReservas(reservas.filter(reserva => reserva._id !== reservaId));
+        } catch (error) {
+          console.error("Error deleting reserva:", error);
+          Swal.fire(
+            'Error!',
+            'Hubo un problema al eliminar la reserva.',
+            'error'
+          );
+        }
+      }
+    });
+  };
+  
+  
+  
+  
+  
+  return (
+    <PortalLayout>
+      <div className="posts">
+        <div className="datosReserva">
+          <Link to="/Dashboard">
+            <Button color="primary">Regresar</Button>
+          </Link>
+          <section class="table__body" >
+            <table>
+              <thead>
+                <tr>
+                  <th>Fecha</th>
+                  <th>Tiempo</th>
+                  <th>Nombre</th>
+                  <th>Numero</th>
+                  <th>Placa</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {reservas.map((reserva) => (
+                  <tr key={reserva._id}>
+                    <td>{reserva.date}</td>
+                    <td>{reserva.time}</td>
+                    <td>{reserva.nombre}</td>
+                    <td className="numeroW">{reserva.telefono}</td>
+                    <td>{reserva.placa}</td>
+                    <td>
+                      <Button
+                        onClick={() => handleCheckout(reserva)}
+                        color="success"
+                      >
+                        <FaFilePdf />
+                        Generar Factura
+                      </Button>{" "}
+                      <Button
+                        onClick={() => handleDelete(reserva._id)}
+                        color="danger"
+                      >
+                        Cancelar
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </section>
+          </div>
+      </div>
+    
+    <Footer />
+    </PortalLayout>
+  );
+};
+
+export default UserReseve;
